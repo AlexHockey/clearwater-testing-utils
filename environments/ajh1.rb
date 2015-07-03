834ed3e815b6b9073372a5da21e493e04fdf3fe8
Alex Hockey <alex.hockey@metaswitch.com>
2015-07-03 10:52:26 +0100

    Add ajh environment

---
+++ environments/ajh1.rb
@@ -0,0 +1,18 @@
+name "ajh1"
+description "Clearwater deployment - ajh1"
+cookbook_versions "clearwater" => "= 0.1.0"
+override_attributes "clearwater" => {
+  "root_domain" => "cw-ngv.com",
+  "availability_zones" => ["us-east-1a"],
+  "repo_server" => "http://repo.cw-ngv.com/latest",
+  "number_start" => "2010000000",
+  "number_count" => 1000,
+  "keypair" => "dogfood-cw-keypair",
+  "keypair_dir" => "~/.chef/",
+  "pstn_number_count" => 0,
+  "gr" => false,
+  "sas_server" => "sas.cw-ngv.com",
+  #"cdiv_as_enabled" => "Y",
+  #"gemini_enabled" => "Y",
+  #"memento_enabled" => "Y",
+}
