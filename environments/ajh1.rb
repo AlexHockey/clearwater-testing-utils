d32f4eaf98a008058c09d2beb3f1210e19200d2d
Alex Hockey <alex.hockey@metaswitch.com>
2015-07-28 12:27:28 +0100

    Update environment

--- environments/ajh1.rb
+++ environments/ajh1.rb
@@ -4,7 +4,7 @@ cookbook_versions "clearwater" => "= 0.1.0"
 override_attributes "clearwater" => {
   "root_domain" => "cw-ngv.com",
   "availability_zones" => ["us-east-1a"],
-  "repo_server" => "http://repo.cw-ngv.com/latest",
+  "repo_server" => "https://repo.cw-ngv.com/~ajh/uplevel",
   "number_start" => "2010000000",
   "number_count" => 1000,
   "keypair" => "dogfood-cw-keypair",
@@ -12,7 +12,7 @@ override_attributes "clearwater" => {
   "pstn_number_count" => 0,
   "gr" => false,
   "sas_server" => "sas.cw-ngv.com",
-  #"cdiv_as_enabled" => "Y",
-  #"gemini_enabled" => "Y",
-  #"memento_enabled" => "Y",
+  "cdiv_as_enabled" => "Y",
+  "gemini_enabled" => "Y",
+  "memento_enabled" => "Y",
 }
