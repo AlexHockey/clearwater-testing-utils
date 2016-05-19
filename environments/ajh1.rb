b634c47a51b3f16ad4864f7a58b15eab30067c21
Alex Hockey <alex.hockey@metaswitch.com>
2016-05-19 15:44:24 +0100

    Updates to chef environment

--- environments/ajh1.rb
+++ environments/ajh1.rb
@@ -1,18 +1,18 @@
 name "ajh1"
 description "Clearwater deployment - ajh1"
-cookbook_versions "clearwater" => "= 0.1.0"
+cookbook_versions "clearwater" => "0.1.0", "clearwater-ccf" => "0.1.0"
 override_attributes "clearwater" => {
   "root_domain" => "cw-ngv.com",
   "availability_zones" => ["us-east-1a"],
-  "repo_server" => "https://repo.cw-ngv.com/~ajh/uplevel",
-  "number_start" => "2010000000",
+  "repo_servers" => ["https://repo.cw-ngv.com/~ajh/current"],
+  "number_start" => "3010000000",
   "number_count" => 1000,
   "keypair" => "dogfood-cw-keypair",
   "keypair_dir" => "~/.chef/",
   "pstn_number_count" => 0,
   "gr" => false,
   "sas_server" => "sas.cw-ngv.com",
-  "cdiv_as_enabled" => "Y",
-  "gemini_enabled" => "Y",
-  "memento_enabled" => "Y",
+  #"cdiv_as_enabled" => "Y",
+  #"gemini_enabled" => "Y",
+  #"memento_enabled" => "Y",
 }
